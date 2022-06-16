09e79b3bed08c09c8ed7270f672de2192a3f90ec
sappysealsz <enchantercode@gmail.com>
2022-06-16 20:06:06 +0200

    Update raptormasternode.py

--- raptormasternode.py
+++ raptormasternode.py
@@ -118,8 +118,8 @@ class RaptorBlockProducer(object):
             except Exception as e:
                 print(f"Exception caught : {e}")
 
-# key used during tests : 47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad
-# this key leads to address 0x6Ff24B19489E3Fe97cfE5239d17b745D4cEA5846
+# key used during tests : 08ee5b2dd065b558af2a27df9989c6e4d01de25c194b83c449807e661c3ea2e2
+# this key leads to address 0xD6dCdcFEde242Ed16dB7Cb97113025d9B6606560
 
 
 nodeaddr = "http://localhost:6969/"
