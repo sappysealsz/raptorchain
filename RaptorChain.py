16b05c2ed17380447f886d7ba12c72cdc53e0d47
sappysealsz <enchantercode@gmail.com>
2022-08-31 15:49:26 +0200

    Change version code to 1.1.0

--- RaptorChain.py
+++ RaptorChain.py
@@ -848,7 +848,7 @@ class State(object):
         self.chainID = 499597202514 if self.testnet else 1380996178
         self.gasPrice = 1000000000000000 # 0.001 RPTR or 1M gwei
         self.burnAddress = "0x000000000000000000000000000000000000dEaD"
-        self.version = "1.0.6-mainnet-beta"
+        self.version = "1.1.0-mainnet-beta"
 
     def formatAddress(self, _addr):
         if (type(_addr) == int):
