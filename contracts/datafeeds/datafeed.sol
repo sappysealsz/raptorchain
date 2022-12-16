0a383a07a2379ecee68f433b17512ed52c2ed1be
sappysealsz <enchantercode@gmail.com>
2022-12-16 23:50:20 +0100

    Update datafeed.sol

--- contracts/datafeeds/datafeed.sol
+++ contracts/datafeeds/datafeed.sol
@@ -9,28 +9,50 @@ pragma solidity 0.7.0;
 contract DataFeed {
 	struct Slot {
 		address owner;
-		bytes32 key;
+		bytes32 variable;	// variable key
 		bytes data;
 		uint256 timestamp;
 		bool written;
 	}
 	
-	mapping (address => mapping (bytes32 => Slot)) public slots;
+	struct Variable {
+		bytes32[] history;
+	}
 	
-	event SlotWritten(address indexed slotOwner, bytes32 indexed slotKey, bytes data);
+	struct User {
+		mapping (bytes32 => Slot) slots;
+		mapping (bytes32 => Variable) variables;
+	}
+	
+	mapping (address => User) users;
+	
+	event SlotWritten(address indexed slotOwner, bytes32 indexed variableKey, bytes32 indexed slotKey, bytes data);
 	
 	function isWritten(address owner, bytes32 key) public view returns (bool) {
-		return slots[owner][key].written;
+		return users[owner].slots[key].written;
 	}
 	
 	function getSlotData(address owner, bytes32 key) public view returns (bytes memory) {
-		return slots[owner][key].data;
+		return users[owner].slots[key].data;
 	}
 	
-	function writeSlot(bytes32 key, bytes memory slotData) public {
-		require(!isWritten(msg.sender, key), "ALREADY_WRITTEN");
-		Slot memory newSlot = Slot({ owner: msg.sender, key: key, data: slotData, timestamp: block.timestamp , written: true });
-		slots[msg.sender][key] = newSlot;
-		emit SlotWritten(msg.sender, key, slotData);
+	function getVariableData(address owner, bytes32 key) public view returns (bytes memory) {
+		User storage user = users[owner];
+		Variable storage _var = user.variables[key];
+		if (_var.history.length == 0) {
+			return "";
+		}
+		return user.slots[_var.history[_var.history.length-1]].data;
+	}
+	
+	function write(bytes32 variableKey, bytes memory slotData) public {
+		bytes32 slotKey = keccak256(abi.encodePacked(variableKey, blockhash(block.number-1)));
+		require(!isWritten(msg.sender, slotKey), "ALREADY_WRITTEN");
+		User storage user = users[msg.sender];
+		Variable storage _var = user.variables[variableKey];
+		_var.history.push(slotKey);
+		Slot memory newSlot = Slot({ owner: msg.sender, variable: variableKey, data: slotData, timestamp: block.timestamp , written: true });
+		user.slots[slotKey] = newSlot;
+		emit SlotWritten(msg.sender, variableKey, slotKey, slotData);
 	}
 }
