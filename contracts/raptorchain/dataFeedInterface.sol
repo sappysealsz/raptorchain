779b34aeacba7a22622e3fe173208e5c22c9737d
sappysealsz <enchantercode@gmail.com>
2022-12-23 14:55:20 +0100

    Update dataFeedInterface.sol

--- contracts/raptorchain/dataFeedInterface.sol
+++ contracts/raptorchain/dataFeedInterface.sol
@@ -2,4 +2,5 @@ pragma solidity ^0.7.0;
 
 interface CrossChainDataFeed {
 	function getSlotData(uint256 chainid, address slotOwner, bytes32 slotKey) external view returns (bytes memory slotData);
+	function crossChainCall(uint256 chainid, address to, uint256 gasLimit, bytes memory data) external;
 }
