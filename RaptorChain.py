cdda1eac55b5aef3e1180878ed7e8dfabec5419f
sappysealsz <enchantercode@gmail.com>
2023-03-07 17:09:35 +0100

    Remove unuseful comment

--- RaptorChain.py
+++ RaptorChain.py
@@ -953,7 +953,6 @@ class State(object):
         self.chainID = 499597202514 if self.testnet else 1380996178
         self.gasPrice = 1000000000000000 # 0.001 RPTR or 1M gwei
         self.burnAddress = "0x000000000000000000000000000000000000dEaD"
-#        self.persistencyUpgradeBlock = 7
         self.version = "1.6.0-mainnet-beta"
 
     def formatAddress(self, _addr):
